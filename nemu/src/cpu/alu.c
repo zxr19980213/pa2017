167fbf653115c7b15f204e9e9b64114e860875d7
tracer-pa2017 <tracer@njuics.org>
2017-09-09 20:50:16 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  20:50:16 up  7:17,  1 user,  load average: 0.24, 0.14, 0.05 78b5a40077ecd278b3bec424d269089abab08183

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -211,7 +211,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
 uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size) {
 	uint32_t res=dest>>src;
 
-    set_CF_shr(src,dest,data_size_size);
+    set_CF_shr(src,dest,data_size);
     set_PF(res);
     set_ZF(res);
     set_SF(res);
