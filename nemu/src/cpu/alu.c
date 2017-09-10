7bbaf19957b6d4ff58dd54912aa5ffd02b2d42d9
tracer-pa2017 <tracer@njuics.org>
2017-09-09 23:09:43 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  23:09:43 up  9:37,  1 user,  load average: 0.24, 0.05, 0.04 36108e779ff6ef3ee475056393719ac314c7ed67

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -151,15 +151,17 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
 }
 
 int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint64_t res=(uint64_t)src*dest;
+    return res;
 }
 
 uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size) {
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	if(src==0){
+        cout<<"motherfucker!"<<endl;
+        return 0;
+    }
+    uint32_t res=dest/src;
+    return res;
 }
 
 int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size) {
