cc7b675256fed3ecf35a0a603b0c85b63e41ce72
tracer-pa2017 <tracer@njuics.org>
2017-09-22 07:57:16 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  07:57:16 up  1:39,  6 users,  load average: 0.01, 0.07, 0.05 2bafa6713d06bfc325a06079736966ecbac8c6d9

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,12 +5,12 @@ make_instr_func(ret_near){
     printf("\n%x\n",cpu.eip);
     temp.type=OPR_MEM;
     temp.data_size=32;
-    temp.addr=cpu.ebp+4;
+    temp.addr=cpu.esp;
     operand_read(&temp);
     printf("\n%x\n",temp.val);
     // int len=temp.val-cpu.eip;
     cpu.eip=temp.val;
-    printf("\n %x\n",cpu.eip);
+    printf("\n%x\n",cpu.eip);
     //printf("%x\n",len+1);
     return 0;
 }
