ae22a065852555a9a5c5ab1ca8f844d4d2176cf7
tracer-pa2017 <tracer@njuics.org>
2017-09-22 07:02:51 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  07:02:51 up 45 min,  6 users,  load average: 0.02, 0.09, 0.08 91ab4a821a1bb3c03fb808391d70008ffb057635

--- nemu/src/cpu/instr/cmp.d
+++ nemu/src/cpu/instr/cmp.d
@@ -8,7 +8,8 @@ src/cpu/instr/cmp.o: src/cpu/instr/cmp.c include/cpu/instr.h \
  include/cpu/instr/jmp.h include/cpu/instr/shift.h \
  include/cpu/instr/flags.h include/cpu/instr/group.h \
  include/cpu/instr/special.h include/cpu/instr/x87.h \
- include/cpu/instr/cmp.h
+ include/cpu/instr/cmp.h include/cpu/instr/push.h \
+ include/cpu/instr/call.h include/cpu/instr/ret.h
 
 include/cpu/instr.h:
 
@@ -57,3 +58,9 @@ include/cpu/instr/special.h:
 include/cpu/instr/x87.h:
 
 include/cpu/instr/cmp.h:
+
+include/cpu/instr/push.h:
+
+include/cpu/instr/call.h:
+
+include/cpu/instr/ret.h:
