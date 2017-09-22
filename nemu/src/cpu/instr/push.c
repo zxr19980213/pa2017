13b01f639f4788dbeb07a95fb11752327c994d7e
tracer-pa2017 <tracer@njuics.org>
2017-09-22 06:31:47 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  06:31:47 up 14 min,  4 users,  load average: 0.20, 0.20, 0.11 57ddab7c22c164631d532b7d8de7130c7463c9fb

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -6,7 +6,7 @@ make_instr_func(push_ebp){
     dst.data_size=data_size;
     dst.addr=cpu.esp;
     dst.val=cpu.ebp;
-    dst.type=OPR_MEM;
+    //dst.type=OPR_MEM;
     //printf("fku\n");
     operand_write(&dst);
     //printf("fkt\n");
