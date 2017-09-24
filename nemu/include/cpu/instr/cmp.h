26807a4126475e566fffe7f152864a44b65248f0
tracer-pa2017 <tracer@njuics.org>
2017-09-24 08:38:22 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  08:38:22 up 14:45, 17 users,  load average: 0.08, 0.07, 0.05 32ee6940334f5485eca3b95fd28c0533a85691

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -1,7 +1,7 @@
 #ifndef _INSERT_CMP_H_
 #define _INSERT_CMP_H
 
-make_instr_func(cmp_eax);
+make_instr_func(cmp_b_rmv);
 make_instr_func(cmp_rv_rmv);
 
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,23 +1,23 @@
 #include "cpu/instr.h"
 
-make_instr_func(cmp_eax){
-    OPERAND imm;
+make_instr_func(cmp_b_rmv){
+    OPERAND imm,rm;
     imm.data_size=8;
-
+    rm.data_size=data_size;
     imm.type=OPR_IMM;
-    imm.addr=eip+2;
+    int len=modrm_rm(eip+1,&rm);
+    imm.addr=eip+1+len;
     operand_read(&imm);
     
     if(data_size==16){
-        uint16_t nimm=(int16_t)(char)imm.val;
-        alu_sub(nimm,cpu.gpr[0]._16);
+        imm.val=(int16_t)(char)imm.val;
     }
     else{
-        uint32_t nimm=(int32_t)(char)imm.val;
-        alu_sub(nimm,cpu.eax);
+        imm.val=(int32_t)(char)imm.val;
     }
+    alu_sub(imm.val,rm.val);
     
-    return 3;
+    return 2+len;
 }
 
 make_instr_func(cmp_rv_rmv){
