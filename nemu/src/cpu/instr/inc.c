65799821850a16af8dfda5fa01f250b018143ece
tracer-pa2017 <tracer@njuics.org>
2017-09-24 08:59:51 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  08:59:51 up 15:07, 17 users,  load average: 0.09, 0.07, 0.04 be0364fef98195fd4d3f527eedaad133197744f

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -5,7 +5,9 @@ make_instr_func(inc_rmv){
     rm.data_size=data_size;
     int len=modrm_rm(eip+1,&rm);
     operand_read(&rm);
+    printf("\ninc:%x\n",rm.val);
     rm.val+=1;
+    printf("\ninc:%x\n",rm.val);
     operand_write(&rm);
     return len+1;
 }
