b4f9796e63c10b1b96e8874d4769a6156dbc93f9
tracer-pa2017 <tracer@njuics.org>
2017-09-23 23:49:23 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  23:49:23 up  7:10, 17 users,  load average: 0.08, 0.08, 0.06 a4bcc4702bfcada12b491a91d41de70394632f2f

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -8,6 +8,7 @@ make_instr_func(add_edx){
     operand_read(&imm);
     if(data_size==16)cpu.gpr[2]._16=alu_add(imm.val,cpu.gpr[2]._16);
     else cpu.edx=alu_add(imm.val,cpu.edx);
+    printf("\nimm.val=%x\n",imm.val);
     return 2+data_size/8;
 }
 
