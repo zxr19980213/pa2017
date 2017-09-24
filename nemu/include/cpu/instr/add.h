e0c78dba392841a4c62576fbc4c1489f854cbd95
tracer-pa2017 <tracer@njuics.org>
2017-09-24 08:23:19 -0400

    >  nemu 161220187 user Linux zxr 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  08:23:19 up 14:30, 17 users,  load average: 0.15, 0.10, 0.05 92e3a070c38621de763e32174f12f99468c6cb11

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -1,7 +1,7 @@
 #ifndef _INSERT_ADD_H_
 #define _INSERT_ADD_H_
 
-make_instr_func(add_iv_rv);
+make_instr_func(add_iv_rmv);
 make_instr_func(add_iv_eax);
 make_instr_func(add_rv_rmv);
 make_instr_func(add_b_rmv);
